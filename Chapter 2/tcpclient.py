3d83b0719bb1c8c4fc672ee28d4588414034683e
mizuiro_rivi <onodaha@gmail.com>
2020-02-04 16:28:22 +0900

    add_file

---
+++ Chapter 2/tcpclient.py
@@ -0,0 +1,17 @@
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+#ソケットオブジェクトの作成
+#AF_INETは標準的なIPv4のアドレスやホスト名を使用することを指していて、SOCK_STREAMはTCPを用いることを示している
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#サーバへの接続
+client.connect((target_host,target_port))
+#データの送信
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+#データの受信
+response = client.recv(4096)
+
+print(response)
